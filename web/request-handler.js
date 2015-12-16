94f335a194923bb81fed8404f43fe1da1bd2ff45
jpanopio <jcpanopio1@gmail.com>
2015-12-16 14:48:04 -0800

    Implement serving static HTML file

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,22 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var url = require('url');
 // require more modules/folders here!
+var filemap = {
+  '/': './public/index.html'
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var pathName = filemap[url.parse(req.url).pathname];
+
+  if(pathName === undefined){
+    res.writeHead(404);
+    res.end();
+  } else {
+    var fileStream = fs.createReadStream(pathName, {
+      encoding: 'utf8'
+    });
+    fileStream.pipe(res);
+  }
 };
