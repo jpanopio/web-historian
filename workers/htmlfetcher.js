11d609bdbbfbf53ed31f6dc1bbf3c1bb3e953d43
jpanopio <jcpanopio1@gmail.com>
2015-12-16 21:01:29 -0800

    implemented htmlfetcher.js

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,17 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers');
+
+// Iterate through sites.txt
+archive.readListOfUrls(function(urlList){
+  _.each(urlList, function(url){
+  // Check if each site is archived
+    archive.isUrlArchived(url, function(is){
+      // If the site is not archived:
+      if(!is){
+        // Send a GET request to that site
+        dowloadUrls([url]);
+      }
+    });
+  });
+});
